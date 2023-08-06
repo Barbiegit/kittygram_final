b5c4958ce718928d6941e03edbfe152158aa35fc
unknown <tostixatovi@gmail.com>
2023-08-06 13:53:24 +0300

    pp

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = (os.getenv("SECRET_KEY", ""),)
 
 DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["158.160.2.118", "127.0.0.1", "localhost", "obami.bounceme.net"]
 
 INSTALLED_APPS = [  
     "django.contrib.admin",
